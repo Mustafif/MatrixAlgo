bce2d3491b2e3103d46f351a47374fcf65466f2b
Mustafif <mustafif0929@gmail.com>
2024-05-01 19:31:32 -0400

    Add optimization flags to build.zig and include matrix.c in shared library

--- build.zig
+++ build.zig
@@ -6,14 +6,29 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const c_flags: [][]const u8 = &.{ "-Wall", "-ffast-math", "-O3", "-Werror", "-Wno-unused-parameter", "-Wno-unused-variable", "-Wno-unused-function", "-Wno-unused-value", "-Wno-unused-label", "-Wno-unused-but-set-variable", "-Wno-unused-const-variable", "-Wno-unused-local-typedefs", "-Wno-unused-macros" };
+
+    const lib = b.addSharedLibrary(.{
+        .name = "libmatalgo",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    lib.addCSourceFile(.{ .file = "src/matrix.c", .flags = c_flags });
+    lib.addIncludePath(b.path("includes"));
+    b.installArtifact(lib);
+
     const exe = b.addExecutable(.{
         .name = "matalgo",
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
-    exe.addCSourceFiles(.{ .root = b.path("src"), .files = &.{ "main.c", "matrix.c" }, .flags = &.{ "-Wall", "-ffast-math", "-O3", "-Werror", "-Wno-unused-parameter", "-Wno-unused-variable", "-Wno-unused-function", "-Wno-unused-value", "-Wno-unused-label", "-Wno-unused-but-set-variable", "-Wno-unused-const-variable", "-Wno-unused-local-typedefs", "-Wno-unused-macros" } });
+    exe.addCSourceFile(.{ .file = "src/main.c", .flags = c_flags });
     exe.linkSystemLibrary("m");
+    exe.linkLibrary(lib);
     exe.addIncludePath(b.path("includes"));
 
     b.installArtifact(exe);
