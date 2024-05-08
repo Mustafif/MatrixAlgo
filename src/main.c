28cb7f1ecc30ea28affe2a6db5f80d875d8f3477
Griffin Ashby <griffinashbyis@gmail.com>
2024-05-08 10:31:33 -0400

    Implemented __M1_mmul() for us!

--- src/main.c
+++ src/main.c
@@ -9,30 +9,15 @@ int main()
     Matrix1 matrix = __M1_new(3, 3);
     Matrix1 matrix_2 = __M1_identity(3);
     
-    //creating dynamic arrays
-    T* ptr;
-    T* ptr_2;
-    ptr = (T*)malloc(sizeof(T)*3);
-    ptr_2 = (T*)malloc(sizeof(T)*3);
+    T nums[] = {2.093,10.090,0.0918};
+    T nums2[] = {0,3.1415,2.9714};
+    T nums3[] = {-11, 3, 1};
+    __M1_setCol(matrix, 0, nums);
+    __M1_setCol(matrix, 2, nums2);
+    __M1_setRow(matrix_2, 1, nums);
+    __M1_setRow(matrix_2, 0, nums3);
+    __M1_setRow(matrix_2, 2, nums2);
 
-    if (ptr == NULL || ptr_2 == NULL) { 
-        printf("Memory not allocated.\n"); 
-    } 
-    else { 
-        // Memory allocated 
-        printf("Memory successfully allocated using "
-            "malloc.\n");
-    }
-
-
-    for (int i = 0; i < matrix.rows; ++i) {
-        ptr[i] = 2.09*i;
-        ptr_2[i] = -1.04*i;
-    }
-
-    //accessing dynamic array using dereferencing
-    __M1_setRow(matrix, 0, ptr); 
-    __M1_setRow(matrix, 2, ptr_2);
 
     //testing multiply by scalar and subtraction functions
     Matrix1 matrix_3 = __M1_mulScalar(__M1_sub(matrix_2, matrix), 3.14);
@@ -53,7 +38,5 @@ int main()
     __M1_free(&matrix_2);
     __M1_free(&matrix_3);
     __M1_free(&matrix_4);
-    free(ptr);
-    free(ptr_2);
     return 0;
 }
